3a741fa93d91d18bd23d452d14ed90ad8cc5dfad
brianadonisosorio <74394656+brianadonisosorio@users.noreply.github.com>
2021-05-17 09:43:32 -0400

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,5 @@
-
 from pathlib import Path
+
 
 class Site:
     def __init__(self, source, dest):
@@ -14,8 +14,7 @@ class Site:
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
-                self.create_dir(path)
-                
+                self.create_dir(path)               
             
 
 
