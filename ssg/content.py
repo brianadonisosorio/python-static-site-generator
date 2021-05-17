d1837de7a15e633451af5c354c39d27200322a5f
brianadonisosorio <74394656+brianadonisosorio@users.noreply.github.com>
2021-05-17 12:29:07 -0400

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -8,6 +8,7 @@ class Content:
     __delimeter = "^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
+    @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
